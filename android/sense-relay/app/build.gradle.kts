c73ac43d1d5b806235c6efd1c24a794ddcdefdaa
kevinjacb <kevingeniard2002@gmail.com>
2026-07-02 12:36:12 +0530

    build(android): add Compose, DataStore, viewmodel-compose deps

--- android/sense-relay/app/build.gradle.kts
+++ android/sense-relay/app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
     id("org.jetbrains.kotlin.plugin.serialization")
+    id("org.jetbrains.kotlin.plugin.compose")
 }
 
 android {
@@ -32,6 +33,8 @@ android {
             isMinifyEnabled = false
         }
     }
+
+    buildFeatures { compose = true }
 }
 
 dependencies {
@@ -39,6 +42,16 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.9.0")
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
     implementation("androidx.core:core-ktx:1.13.1")
+
+    val composeBom = platform("androidx.compose:compose-bom:2024.10.01")
+    implementation(composeBom)
+    implementation("androidx.compose.ui:ui")
+    implementation("androidx.compose.ui:ui-tooling-preview")
+    implementation("androidx.compose.material3:material3")
+    implementation("androidx.activity:activity-compose:1.9.3")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.7")
+    implementation("androidx.datastore:datastore-preferences:1.1.1")
+    debugImplementation("androidx.compose.ui:ui-tooling")
 
     testImplementation(kotlin("test"))
 
